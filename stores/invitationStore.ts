e66641aa8b5946e27ce7f0f57f55f93d308bb4f7
Antonio Dell'Annunziata <contact@antonio-da.dev>
2024-11-24 11:09:29 +0100

    Fix serialization of invitation in local storage

--- stores/invitationStore.ts
+++ stores/invitationStore.ts
@@ -3,11 +3,22 @@ export const useInvitationStore = defineStore("invitations", () => {
 
   onMounted(() => {
     const storedInvitation = localStorage.getItem("invitation");
-    if (storedInvitation) invitation.value = JSON.parse(storedInvitation);
+    if (
+      storedInvitation &&
+      storedInvitation !== "undefined" &&
+      storedInvitation.includes("code") &&
+      storedInvitation.includes("id")
+    )
+      invitation.value = JSON.parse(storedInvitation);
+    else {
+      invitation.value = undefined;
+      localStorage.clear();
+    }
   });
 
   watch(invitation, (newValue) => {
-    localStorage.setItem("invitation", JSON.stringify(newValue));
+    if (newValue) localStorage.setItem("invitation", JSON.stringify(newValue));
+    else localStorage.clear();
   });
 
   return { invitation };
